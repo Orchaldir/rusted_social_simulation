5056554bd7672283b515d09607831da363fd64cc
Orchaldir <orchaldir@gmail.com>
2020-10-12 21:02:40 +0200

    [#3] Add ConditionalUtility

--- rusted_social_simulation/src/social/utility.rs
+++ rusted_social_simulation/src/social/utility.rs
@@ -1,3 +1,5 @@
+use crate::social::condition::Condition;
+
 /// A utility rule can be used to calculate the utility of something (e.g. an action) for a given context.
 pub trait UtilityRule<T> {
     fn calculate_utility(&self, context: &T) -> i32;
@@ -9,8 +11,8 @@ pub struct FixedUtility {
 }
 
 impl FixedUtility {
-    pub fn new(value: i32) -> FixedUtility {
-        FixedUtility { utility: value }
+    pub fn new(utility: i32) -> FixedUtility {
+        FixedUtility { utility }
     }
 }
 
@@ -23,5 +25,38 @@ impl<T> UtilityRule<T> for FixedUtility {
     /// ```
     fn calculate_utility(&self, _: &T) -> i32 {
         self.utility
+    }
+}
+
+/// An utility rule with an utility based on a condition.
+pub struct ConditionalUtility<T> {
+    condition: Box<dyn Condition<T>>,
+    utility: i32,
+}
+
+impl<T> ConditionalUtility<T> {
+    pub fn new(condition: Box<dyn Condition<T>>, utility: i32) -> ConditionalUtility<T> {
+        ConditionalUtility { condition, utility }
+    }
+}
+
+impl<T> UtilityRule<T> for ConditionalUtility<T> {
+    /// Returns a fixed utility or 0 based on a condition.
+    ///
+    /// ```
+    ///# use rusted_social_simulation::social::condition::*;
+    ///# use rusted_social_simulation::social::utility::{ConditionalUtility, UtilityRule};
+    /// let with_false = ConditionalUtility::new(Box::new(MockCondition::new(false)), 35);
+    /// let with_true = ConditionalUtility::new(Box::new(MockCondition::new(true)), 78);
+    ///
+    /// assert_eq!(with_false.calculate_utility(&42), 0);
+    /// assert_eq!(with_true.calculate_utility(&42), 78);
+    /// ```
+    fn calculate_utility(&self, context: &T) -> i32 {
+        if self.condition.evaluate(context) {
+            self.utility
+        } else {
+            0
+        }
     }
 }
