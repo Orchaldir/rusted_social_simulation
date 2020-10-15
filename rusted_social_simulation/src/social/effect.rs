e5278e0242a7dd3f888be71316cad8d5288204f2
Orchaldir <orchaldir@gmail.com>
2020-10-15 23:23:27 +0200

    [#4] Add MockEffect

--- rusted_social_simulation/src/social/effect.rs
+++ rusted_social_simulation/src/social/effect.rs
@@ -18,6 +18,32 @@ impl<T> Effect<T> for DoNothing {
     fn apply(&self, _: &mut T) {}
 }
 
+/// A mock effect for testing.
+pub struct MockEffect {
+    value: u32,
+}
+
+impl MockEffect {
+    pub fn new(value: u32) -> MockEffect {
+        MockEffect { value }
+    }
+}
+
+impl Effect<u32> for MockEffect {
+    /// Modify the context for easy validating that the effect was called.
+    ///
+    /// ```
+    ///# use rusted_social_simulation::social::effect::{Effect, MockEffect};
+    /// let mut context = 42;
+    /// let effect =  MockEffect::new(11);
+    /// effect.apply(&mut context);
+    /// assert_eq!(context, 53);
+    /// ```
+    fn apply(&self, context: &mut u32) {
+        *context += self.value
+    }
+}
+
 /// An effect that consists of multiple sub-effects.
 pub struct EffectVector<T> {
     effects: Vec<Box<dyn Effect<T>>>,
@@ -31,8 +57,20 @@ impl<T> EffectVector<T> {
 
 impl<T> Effect<T> for EffectVector<T> {
     /// Apply multiple sub-effects.
+    ///
+    /// ```
+    ///# use rusted_social_simulation::social::effect::{MockEffect, Effect, EffectVector};
+    /// let mut context = 42;
+    /// let effect0 = MockEffect::new(2);
+    /// let effect1 = MockEffect::new(34);
+    /// let vector = EffectVector::new(vec![Box::new(effect0), Box::new(effect1)]);
+    ///
+    /// vector.apply(&mut context);
+    ///
+    /// assert_eq!(context, 78);
+    /// ```
     fn apply(&self, context: &mut T) {
-        for effect in  &self.effects {
+        for effect in &self.effects {
             effect.apply(context)
         }
     }
