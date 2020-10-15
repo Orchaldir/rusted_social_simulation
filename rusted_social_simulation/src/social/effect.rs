4fdbedb7ea436298ad7ebb0b12bca5e02d252e92
Orchaldir <orchaldir@gmail.com>
2020-10-15 20:05:46 +0200

    [#4] Add EffectVector

--- rusted_social_simulation/src/social/effect.rs
+++ rusted_social_simulation/src/social/effect.rs
@@ -17,3 +17,23 @@ impl<T> Effect<T> for DoNothing {
     /// ```
     fn apply(&self, _: &mut T) {}
 }
+
+/// An effect that consists of multiple sub-effects.
+pub struct EffectVector<T> {
+    effects: Vec<Box<dyn Effect<T>>>,
+}
+
+impl<T> EffectVector<T> {
+    pub fn new(effects: Vec<Box<dyn Effect<T>>>) -> EffectVector<T> {
+        EffectVector { effects }
+    }
+}
+
+impl<T> Effect<T> for EffectVector<T> {
+    /// Apply multiple sub-effects.
+    fn apply(&self, context: &mut T) {
+        for effect in  &self.effects {
+            effect.apply(context)
+        }
+    }
+}
